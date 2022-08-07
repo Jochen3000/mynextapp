f7bc676a7251269b82d9fb5361592db92ae9274e
jochen ade <jochen.ade@springer.com>
2022-08-07 15:42:08 +0200

    layout added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import Image from 'next/image'
+import Link from 'next/link'
+
 
 export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi consequatur tempora voluptatem in cum nihil et aliquid nulla, blanditiis voluptates assumenda doloremque mollitia incidunt eaque, labore quisquam excepturi odit autem?</p>
+      <Link href='events'><a>Go to events</a></Link>
     </div>
   )
 }
