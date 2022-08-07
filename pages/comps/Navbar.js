1144147c88dd38a9f88e2fbdf6f7d14583d38a31
jochen ade <jochen.ade@springer.com>
2022-08-07 16:00:55 +0200

    nav highlighting

--- pages/comps/Navbar.js
+++ pages/comps/Navbar.js
@@ -1,14 +1,22 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 const Navbar = () => {
+    const router = useRouter();
     return (
         <nav>
             <div className="logo">
-                <h1>Ninja List</h1>
+                <h1>List</h1>
             </div>
-            <Link href='/'><a>Home</a></Link>
-            <Link href='/about'><a>About</a></Link>
-            <Link href='/events'><a>Events</a></Link>
+            <div className={router.pathname == "/" ? "active" : ""}>
+                <Link href='/'><a>Home</a></Link>
+            </div>
+            <div className={router.pathname == "/about" ? "active" : ""}>
+                <Link href='/about'><a>About</a></Link>
+            </div>
+            <div className={router.pathname == "/events" ? "active" : ""}>
+                <Link href='/events'><a>Events</a></Link>
+            </div>
         </nav>
     );
 }
