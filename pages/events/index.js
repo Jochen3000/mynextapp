61aac5e5c2268194159cde9ca1a6d2cbf64f0ef4
jochen ade <jochen.ade@hotmail.com>
2022-08-09 10:30:31 +0200

    application fully working

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,29 @@
-const Events = () => {
+import Link from "next/link";
+
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: { users: data }
+    }
+}
+
+const Events = ({ users }) => {
     return (
         <div>
             <h1>Events</h1>
+            {users.map(user => (
+                <div key={user.id}>
+                    <Link href={'/events/' + user.id}>
+                        <a className='single-post'>
+                            <h3>{user.name}</h3>
+                        </a>
+                    </Link>
+                </div>
+            ))}
         </div>
     );
 }
 
-export default Events; <div>
-</div>
+export default Events
